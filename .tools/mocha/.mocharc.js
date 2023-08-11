10f37742386dae87cd16be988642d4e22a670470
Benedikt Sauer <sauer.benedikt@outlook.de>
2023-08-11 16:35:08 +0200

    fix: update mocha tests to newest version (#18)

---
+++ .tools/mocha/.mocharc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  require: ['test/mocha.setup.js'],
+  spec: [
+    '!**/(node_modules|test)/**/*.test.js',
+    '*.test.js',
+    'test/**/test!(PackageFiles|Startup).js'
+  ]
+};
